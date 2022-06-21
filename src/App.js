a30eeda7b6d88362dd88c18f3fa48d5d816c9fbe
Jessica Jolly <jessicajolly4416@gmail.com>
2022-06-21 22:35:42 +0530

    Repositioned the navbar and created a temporary dark bg

--- src/App.js
+++ src/App.js
@@ -4,20 +4,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="App">
-    <Navbar bg="dark" variant="dark">
-    <Container>
-    <Navbar.Brand href="#home">PC Website</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    </Container>
-  </Navbar>
+      <Helmet>
+        <style>{"body { background-color: #282c34; }"}</style>
+      </Helmet>
+      <Container>
+      <Navbar className="App-nav" sticky="top" expand="lg" bg="light" variant="light">
+      <Container>
+      <Navbar.Brand href="#home">PC Website</Navbar.Brand>
+      <Nav className="ml-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#applications">Applications</Nav.Link>
+        <Nav.Link href="#profile">Profile</Nav.Link>
+      </Nav>
+      </Container>
+      </Navbar>
+      </Container>
     </div>
   );
 }
