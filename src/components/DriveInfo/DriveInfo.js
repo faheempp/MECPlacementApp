88b1c9e0d340b6ab3da37036184a34fc4348ddf0
Faheem <faheempp@outlook.com>
2022-07-16 19:44:06 +0530

    drive info card design update

--- src/components/DriveInfo/DriveInfo.js
+++ src/components/DriveInfo/DriveInfo.js
@@ -4,16 +4,19 @@ import './DriveInfo.css';
 function DriveInfo(){
     return(
         <div className="driveinfo">
-          <div className="side-card">
+          <div className="left-card">
+            <h3>Company Name</h3>
+            <h2>Post: </h2>
+            <div>
+              <p>Other details</p>
+            </div>
+            <button>View</button>
+          </div>
+          <div className="right-card">
             <p className="requirements">Requirements</p>
             <p className="last-date">Last Date</p>
           </div>
-          <h3>Company Name</h3>
-          <h2>Post: </h2>
-          <div>
-            <p>Other details</p>
-          </div>
-          <button>View</button>
+          
           
         </div>
     )
