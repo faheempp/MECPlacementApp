4db1dcfb15473d02b9d72ca7156633e70d5cd55e
Faheem <faheempp@outlook.com>
2022-07-17 11:41:15 +0530

    header update

---
+++ src/components/Header/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import './Header.css';
+
+import { Link } from 'react-router-dom'
+import  collegelogo from'../../images/collegelogo.png';
+
+
+function Header({active}){
+    return(
+        <div className="header">
+            <div className="logotitlecontainer">
+                {/* <div className="logo">
+                    <img src={collegelogo} alt=""></img>
+                </div> */}
+                <h2 className="pctitle">Placement Cell</h2>
+                {/* <div className="searchbar">Searchbars</div> */}
+            </div>
+            <div className="nav">
+
+                
+                <Link to="/" className="nav-item">Home</Link>
+                <Link to="/application" className="nav-item">Application</Link>
+                <Link to="/profile" className="nav-item">Profile</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
