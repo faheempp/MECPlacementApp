ee50457e6f64b633347887e0812c877b083e0894
Elizabeth Mathew <62603419+Elizabeth-Mathew1@users.noreply.github.com>
2022-07-27 22:19:34 +0530

    User Auth Performed

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,6 +4,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import {initializeApp} from "firebase/app"
 import { getFirestore } from 'firebase/firestore';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBGxtfAReJN3vQI6oXZf4G5TE4gCiO8hI8",
@@ -14,10 +15,34 @@ const firebaseConfig = {
   appId: "1:371778176262:web:5bd6cd650c2bae66e30a87"
 };
 
+
+
 // Initialize Firebase
 
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 //const auth = firebase.auth();
+
+// User authentication with google
+
+export const auth = getAuth(app);
+export const provider= new GoogleAuthProvider()
+export const SignInWithGoogle=()=>
+  {
+   signInWithPopup(auth, provider).then ((result)=> {
+    const name=result.user.displayName;
+    const email=result.user.email;
+    const photo=result.user.photoURL;
+
+    localStorage.setItem("Name", name)
+    localStorage.setItem("Email", email)
+    localStorage.setItem("Photo", photo)
+
+    console.log(result)
+
+   }).catch((error) =>{
+     console.log(error);
+   })
+ }
 export { db, app }
