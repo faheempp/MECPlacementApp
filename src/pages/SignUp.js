7bce070fc6404d0097e2f0e4e4b359fd7e9f895d
Jessica Jolly <jessicajolly4416@gmail.com>
2022-07-30 19:05:55 +0530

    modified the signUp class and added visibility icons

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,61 +1,65 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
-import Header from '../components/Header/Header';
+import React, {useState } from 'react'
+import {AiFillEyeInvisible, AiFillEye} from 'react-icons/ai'
+import { Link } from 'react-router-dom'; 
 import './SignIn.css'
-import  { SignInWithGoogle } from '../firebase-config'
-export class SignUp extends Component {
-  state={
-        email:'',
-        password:'',
-        firstName:'',
-        lastName:''
+import  { SignInWithGoogle } from '../firebase-config' 
+
+export default function SignUp(){ 
+    //handle password eye
+    const [passwordEye, setPasswordEye]=useState(false);
+    const [conf_passwordEye, setConfPasswordEye]=useState(false);
+    const [email, setEmail]=useState('');
+    const [password, setPassword]=useState('');
+
+    const handlePasswordClick = () => {
+        setPasswordEye(!passwordEye)
     }
-    handleChange=(e)=>{
-        this.setState({
-            [e.target.id]: e.target.value,
-        })
+    const handleConfPasswordClick = () => {
+        setConfPasswordEye(!conf_passwordEye)
+    }  
+    const handleChangeEmail=(e)=>{
+        setEmail(e.value) 
     }
-    
-    handleSubmit=(e)=>{
+    const handleChangePassword=(e)=>{
+        setPassword(e.value) 
+    }
+    const handleSubmit=(e)=>{
         e.preventDefault();
-        console.log(this.state);
-    }
-  render() {
+        console.log(password);
+    } 
     return (
       <div className='signin-page-container'>
         <div className='title-section'>
             <h1 className='signinpage-pctitle'>Placement Cell</h1>
         </div>
         <div className='signin-form-container'>
-            <form onSubmit={this.handleSubmit}  className='sa'>
-            <h5 className='signin-heading'>Sign Up</h5>
-            
-            {/* <div className='input-field'>
-                <label htmlFor='firstName'>First Name</label>
-                <input type="text" id="firstName"  onChange={this.handleChange}></input>
-            </div>
-            
-            <div className='input-field'>
-                <label htmlFor='lastName'>Last Name</label>
-                <input type="text" id="lastName"  onChange={this.handleChange}></input>
-            </div> */}
+            <form  onSubmit={handleSubmit} className='sa'> 
             <div className='input-field'>
                 <label htmlFor='email'>Email</label>
-                <input type="email" id="email" onChange={this.handleChange}></input>
+                <input type="email" id="email" onChange={handleChangeEmail}></input>
             </div>
             <div className='input-field'>
                 <label htmlFor='password'>Password</label>
-                <input type="password" id="password"  onChange={this.handleChange}></input>
+                <input type={(passwordEye === false)?"password":"text"} id="password" onChange={handleChangePassword}></input> 
+                    {
+                        (passwordEye === false)? <AiFillEyeInvisible className='visibility-icon' onClick={handlePasswordClick}/>:
+                        <AiFillEye className='visibility-icon' onClick={handlePasswordClick}/>
+                    }  
+            </div>
+            <div className='input-field'>
+                <label htmlFor='confirm-password'>Confirm Password</label>
+                <input type={(conf_passwordEye === false)?"password":"text"} id="conf-password" ></input> 
+                    {
+                        (conf_passwordEye === false)? <AiFillEyeInvisible className='visibility-icon' onClick={handleConfPasswordClick}/>:
+                        <AiFillEye className='visibility-icon' onClick={handleConfPasswordClick}/>
+                    }  
             </div>
             <div className='input-field signin-button-container'>
-                <button className='signin-button' onClick={ SignInWithGoogle }>Sign Up</button>
+                <button className='signin-button' onClick={ handleSubmit }>Sign Up</button>
             </div>
         </form>
         <div className='formfooter' >Already have an account? <Link to="/signin" className="">Sign In</Link></div>
         </div>
       </div>
     )
-  }
 }
-
-export default SignUp
