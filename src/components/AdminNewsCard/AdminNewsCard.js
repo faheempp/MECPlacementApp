3b795f7923da3e7494263a906f25f9d6e27bb618
Jessica Jolly <jessicajolly4416@gmail.com>
2022-07-31 19:58:00 +0530

    added confirmation on deletion

--- src/components/AdminNewsCard/AdminNewsCard.js
+++ src/components/AdminNewsCard/AdminNewsCard.js
@@ -6,11 +6,17 @@ export default function AdminNewsCard(props) {
   const newscontent=props.newsContent;
   const newsId=props.newsId;
   const [message,setMessage]=useState({error:false,msg:""});
+  const confirmDeletion=async()=>{
+    if(window.confirm('Are you sure you want to Delete this item?')){
+      deleteNews();
+    }
+  }
   const deleteNews=async()=>{
     try{
-            await NewsDataService.deleteNews(newsId);
+            await NewsDataService.deleteNews(newsId); 
             window.location.reload()
             setMessage({error:false,msg:"News(Id:${newsId}) Deleted"});
+            window.alert(message);
         }
         catch(err){
             setMessage({error:true,msg:err.message});
@@ -21,7 +27,7 @@ export default function AdminNewsCard(props) {
     <div className='adm-news-card'>
         <h4 className='adm-news-heading'>{newsheading}</h4>
         <div className='adm-news-content'>{newscontent}</div>
-        <button className='del-button' onClick={deleteNews}>Delete</button>
+        <button className='del-button' onClick={confirmDeletion}>Delete</button>
     </div>
   )
 }
