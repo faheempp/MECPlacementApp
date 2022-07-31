353a3f331ba3a208c94f1467826de4485860a4d1
Jessica Jolly <jessicajolly4416@gmail.com>
2022-07-31 19:32:44 +0530

    Updated all pages with footer

--- src/pages/Application.js
+++ src/pages/Application.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
 import ApplicationPageContainer from '../components/ApplicationPageContainer/ApplicationPageContainer';
 export default function Application() {
   return (
     <div className='application-page'>
         <Header/>
         <ApplicationPageContainer/>
+        <Footer/>
     </div>
   )
 }
