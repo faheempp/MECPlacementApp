d61dcc68adc81302d61f97a57ac5e2cfee45b8d4
Jessica Jolly <jessicajolly4416@gmail.com>
2022-08-03 19:34:40 +0530

    dynamic admin header

--- src/pages/AdminApplicants.js
+++ src/pages/AdminApplicants.js
@@ -5,7 +5,7 @@ import Footer from '../components/Footer/Footer';
 export default function AdminApplicants() {
   return (
     <div>
-        <AdminHeader/>
+        <AdminHeader active='applicants'/>
         <ApplicantsPageContainer/>
     </div>
   )
